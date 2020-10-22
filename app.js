d8acb50e84c0b0edd256f32573412294806665ca
mnava1418 <mnavapena@gmail.com>
2020-10-22 07:04:24 -0500

    set hbs helpers

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+let app = express();
 
 // view engine setup
+hbs.registerPartials(path.join(__dirname, "views/partials"))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
