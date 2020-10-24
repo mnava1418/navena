3c9f06adf0096df93782de2bfaf7031245cc18be
mnava1418 <mnavapena@gmail.com>
2020-10-23 20:03:20 -0500

    responsive styles

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -36,12 +36,6 @@ if (
       // Must change focus!
       var $target = $(target);
       $target.focus();
-      if ($target.is(":focus")) { // Checking if the target was focused
-        return false;
-      } else {
-        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-        $target.focus(); // Set focus again
-      };
     });
   }
 }
