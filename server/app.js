113543faae572e5f912101509bc6d2f7db1392bd
mnava1418 <mnavapena@gmail.com>
2020-10-26 17:50:44 -0600

    use body parder

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index');
 
@@ -18,7 +19,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static('public'));
 
 app.use('/', indexRouter);
 
